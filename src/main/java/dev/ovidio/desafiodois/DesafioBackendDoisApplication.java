ce38dc806f57dd8aff3bf3cd3a40b1a62b064bad
André Luís Ovidio <andreovidio55@gmail.com>
2023-08-09 22:12:08 -0300

    feat: init do projeto spring

---
+++ src/main/java/dev/ovidio/desafiodois/DesafioBackendDoisApplication.java
@@ -0,0 +1,13 @@
+package dev.ovidio.desafiodois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendDoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendDoisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ovidio/desafiodois/DesafioBackendDoisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ovidio.desafiodois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendDoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
